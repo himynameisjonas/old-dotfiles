8978d981f2d6b4ba1a4cba288b0903be43d99d76
Jonas Forsberg <himynameisjonas@gmail.com>
2011-05-20 19:46:50 +0200

    moved around files a bit

--- bash/completion_scripts/site_aliases.rb
+++ bash/completion_scripts/site_aliases.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-Dir["#{File.expand_path("~/Sites/rails")}/*"].each { |file|
+Dir["#{File.expand_path("~/code")}/*"].each { |file|
     if File.directory?(file)
         cmd=File.basename(file)
         puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
