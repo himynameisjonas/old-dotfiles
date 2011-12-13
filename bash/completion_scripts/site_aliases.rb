befa731f809b7ca77984539a66e8a59c245242cf
Jonas Forsberg <himynameisjonas@gmail.com>
2011-12-13 21:20:13 +0100

    fixed code/* aliases

--- bash/completion_scripts/site_aliases.rb
+++ bash/completion_scripts/site_aliases.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
-Dir["#{File.expand_path("~/code")}/*"].each { |file|
-    if File.directory?(file)
-        cmd=File.basename(file)
-        puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
-    end
-}
+# Dir["#{File.expand_path("~/code")}/*"].each { |file|
+#     if File.directory?(file)
+#         cmd=File.basename(file)
+#         puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
+#     end
+# }
